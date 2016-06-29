2953ca00f5614d8f8068045f5b21a5cf4ea35f9f
bobcui <bobcui@yeah.net>
2016-06-30 00:17:45 +0800

    dev backup
    potato mysql handler in develop

--- app/servers/potato/handler/mysql.js
+++ app/servers/potato/handler/mysql.js
@@ -1,3 +1,7 @@
+var _ = require('lodash')
+var async = require('async')
+var logger = require('pomelo-logger').getLogger('potato', __filename, 'pid:'+process.pid)
+
 module.exports = function(app) {
   return new Handler(app)
 }
@@ -7,11 +11,89 @@ var Handler = function(app) {
 }
 
 Handler.prototype.query = function(req, session, cb) {
-  this.app.rpc.mysql.remote.query(this.mysql, req.sql, req.values, req.timeout, cb)
+  this.app.rpc.mysql.remote.query.toServer(req.serverId, req.sql, req.values, req.timeout, function(err, result){
+    if (!!err) {
+      logger.error('mysql.remote.query error. serverId=%s err=%s', req.serverId, err.stack)
+      cb(null, {err:'INTERNAL_SERVER_ERROR'})
+    }
+    else {
+      cb(null, {err:null, result:result})
+    }
+  })
 }
 
+// req: {
+//   sql:
+//   timeout:
+//   values: {
+//     key: []
+//   }
+//   serverIds: {
+//     key: serverId
+//   }
+// }
+// res: {
+//   key1: {
+//     err:
+//     result:
+//   }
+//   key2: ...
+// }
 Handler.prototype.queryMultiValue = function(req, session, cb) {
-  this.app.rpc.mysql.remote.queryMultiValue(this.mysql, req.sql, req.values, req.timeout, cb)
+  var app = this.app,
+    sql = req.sql,
+    timeout = req.timeout,
+    values = req.values,
+    serverIds = req.serverIds,
+    serverValues = {}
+
+
+  // serverValues: {
+  //   serverId1: {
+  //     key1: values1
+  //     key2: values2
+  //   }
+  //   serverId2: ...
+  // }
+  _.each(serverIds, function(serverId, key){
+    if (_.isNil(serverValues[serverId])) {
+      serverValues[serverId] = {}
+    }    
+    serverValues[serverId][key] = values[key]
+  })
+
+  var funcs = {}
+  _.each(serverValues, function(serverId, serverValue){
+    funcs[serverId] = function(callback) {
+      app.rpc.mysql.remote.queryMultiValue.toServer(serverId, sql, serverValue, timeout, function(err, result){
+        if (!!err) {
+          logger.error('mysql.remote.queryMultiValue error. serverId=%s err=%s', serverId, err.stack)
+          result = {}
+          _.each(serverValue, function(v, k){
+            result[k] = {err:'INTERNAL_SERVER_ERROR'}
+          })
+        }
+        callback(null, result)
+      })
+    }
+  })
+
+  // results: {
+  //   serverId1: {
+  //     key: {
+  //       err: 
+  //       result:
+  //     }
+  //   },
+  //   serverId2
+  // }
+  async.parallel(funcs, function(err, results){
+    var finalResults = {}
+    _.each(results, function(result){
+      finalResults = _.merge(finalResults, result)
+    })
+    cb(null, finalResults)
+  })
 }
 
 Handler.prototype.queryMultiSql = function(req, session, cb) {
--- test/others/lodash.js
+++ test/others/lodash.js
@@ -4,3 +4,15 @@ console.log('_.isNil(0) = ' + _.isNil(0))
 console.log('_.isNil(void(0)) = ' + _.isNil(void(0)))
 console.log('_.isNil(null) = ' + _.isNil(null))
 console.log('_.isNil(undefined) = ' + _.isNil(undefined))
+
+console.log(_.union([2], [1, 2]))
+
+var users = {
+  'data': [{ 'user': 'barney' }, { 'user': 'fred' }]
+};
+
+var ages = {
+  'data': [{ 'age': 36 }, { 'age': 40 }]
+};
+
+console.log(_.merge(users, ages))
